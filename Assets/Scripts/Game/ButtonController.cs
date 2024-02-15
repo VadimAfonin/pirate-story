efae068d393977cea005957995f8499756890919
Vadim Afonin <sfyt6554@gmail.com>
2024-02-15 19:34:10 +0300

    Project Builded

--- Assets/Scripts/Game/ButtonController.cs
+++ Assets/Scripts/Game/ButtonController.cs
@@ -44,4 +44,9 @@ public class ButtonController : MonoBehaviour
         Time.timeScale = 1;
         Statistics.RefreshStats();
     }
+
+    public void OnExitButtonClick()
+    {
+        Application.Quit();
+    }
 }
